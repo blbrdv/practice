5642442b5d270502cb023fd50e2c2be20560483f
blbrdv <95272917+blbrdv@users.noreply.github.com>
2023-11-07 14:52:18 +0700

    implement database

--- protohackers.com/4 Unusual Database Program/Server.cs
+++ protohackers.com/4 Unusual Database Program/Server.cs
@@ -1,9 +1,77 @@
-﻿namespace UnusualDatabaseProgram;
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace UnusualDatabaseProgram;
 
 internal static class Server
 {
+    private static readonly Dictionary<string, string> Data = new()
+    {
+        { "version", "Unusual Database Program 1.0" }
+    };
+    
     internal static async Task Run()
     {
-        throw new NotImplementedException();
+        var listener = new UdpClient(5003);
+        
+        Console.WriteLine("Server started");
+        
+        try
+        {
+            while (true)
+            {
+                var result = await listener.ReceiveAsync();
+                _ = Task.Run(() => HandleConnection(listener, result));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            listener.Close();
+        }
+    }
+
+    private static async Task HandleConnection(UdpClient client, UdpReceiveResult result)
+    {
+        var id = Id.New();
+        
+        Console.WriteLine($"{id} | Connected |");
+
+        try
+        {
+            var message = Encoding.ASCII.GetString(result.Buffer);
+        
+            Console.WriteLine($"{id} | Received | {message} |");
+
+            if (message.Contains("="))
+            {
+                var resultMessage = Regex.Matches(message, @"^([^=]*)=(.*)$", RegexOptions.IgnoreCase);
+                var key = resultMessage[0].Groups[1].Value;
+                var value = resultMessage[0].Groups[2].Value;
+
+                if ("version".Equals(key))
+                    return;
+
+                Data[key] = value;
+            }
+            else
+            {
+                var value = Data.GetValueOrDefault(message) ?? string.Empty;
+                var resultMessage = $"{message}={value}";
+                var resultBytes = Encoding.ASCII.GetBytes(resultMessage);
+                await client.SendAsync(resultBytes, result.RemoteEndPoint);
+                
+                Console.WriteLine($"{id} | Sending | {resultMessage} |");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{id} | Error | {e.Message} |");
+        }
     }
 }
